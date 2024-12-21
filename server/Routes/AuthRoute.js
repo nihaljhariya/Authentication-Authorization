6a3eeaf4af21d639e46b3c6d4b2c7b234a572819
nihaljhariya <nihaljha079@gmail.com>
2024-12-22 01:08:42 +0530

    done with respect

---
+++ server/Routes/AuthRoute.js
@@ -0,0 +1,16 @@
+const { Signup, Login } = require('../Controllers/AuthController')
+const router = require('express').Router()
+const express = require('express');
+const userVerification = require("../Middlewares/AuthMiddleware");
+
+router.post('/signup', Signup)
+router.post('/login', Login)
+router.post('/',userVerification)
+
+module.exports = router
+
+
+
+
+
+
